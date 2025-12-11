bda56a1eac71e49b133fc463fbdd4d0dd8fc6372
Zuwa Entertaiment <entertaimentzuwa666@gmail.com>
2025-12-12 02:42:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys"
+import qrcode from "qrcode-terminal"
+
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState("auth_info")
+
+    const sock = makeWASocket({
+        printQRInTerminal: true,
+        auth: state
+    })
+
+    sock.ev.on("creds.update", saveCreds)
+
+    sock.ev.on("connection.update", ({ connection, qr }) => {
+        if (qr) {
+            qrcode.generate(qr, { small: true })
+        }
+        if (connection === "open") console.log("BOT IMEWASHWA ✔")
+        if (connection === "close") console.log("Connection imefungwa ❌")
+    })
+
+    sock.ev.on("messages.upsert", async ({ messages }) => {
+        const msg = messages[0]
+
+        if (!msg.message) return
+
+        const from = msg.key.remoteJid
+        const text = msg.message.conversation || ""
+
+        if (text.toLowerCase() === "hi") {
+            await sock.sendMessage(from, { text: "Hello! Mimi ni bot wa Zuwa Entertainment 😎" })
+        }
+
+        if (text.toLowerCase() === "menu") {
+            await sock.sendMessage(from, { text: "📌 *MENU YA BOT*\n1. Hi\n2. Menu\n" })
+        }
+    })
+}
+
+startBot()
